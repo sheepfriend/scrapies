73aa2d80e04d8faae385770874e36016dda74670
Kelly <xingyue.kelly@gmail.com>
2014-07-30 01:11:44 +0800

    upload new files and change file names

---
+++ renren_getimage.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+import requests
+import re
+import urllib
+from bs4 import BeautifulSoup
+def getImage(http):
+	soup=BeautifulSoup(http)
+	temp=soup.find_all('a',{'class':'picture'})
+	total=[]
+	counts=0
+	for item in temp:
+		total.insert(counts,item['href'])
+		counts+=1
+	return total
+
+def saveImage(http,count):
+	soup=BeautifulSoup(http)
+	temp=soup.find_all('img',{'id':'photo'})
+	for item in temp:
+		urllib.urlretrieve(item['src'],"picture%s.jpg"%count)
+		count+=1
+		print count
+	return count
+	
+s=requests.Session()
+headers ={"User-agent":"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1"}
+logininfo={"email":"******","password":"******"}
+r=s.post("http://www.renren.com/PLogin.do",data=logininfo,headers=headers)
+r2=s.get("http://photo.renren.com/photo/*********/album/relatives") ## ********:friend's id
+images=getImage(r2.text)
+count=0
+for item in images:
+	r=s.get(item)
+	count=saveImage(r.text,count)
---
+++ weather_data.py
@@ -0,0 +1,45 @@
+import urllib
+from bs4 import BeautifulSoup
+import json
+
+year=range(2005,2014)
+month=range(1,13)
+record={}
+
+def get_table(y,m):
+	adr='http://www.usclimatedata.com/climate/pampa/texas/united-states/ustx1017/'+str(y)+'/'+str(m)
+	http=urllib.urlopen(adr)
+	text=http.read()
+	soup=BeautifulSoup(text)
+	text=soup.find('div',{'id':'history_data'})
+	soup=BeautifulSoup(str(text))
+	day=soup.find_all('td',{'class':'align_left daily_climate_table_td_day'})
+	high=soup.find_all('td',{'class':'align_right climate_table_data_td temperature_red '})
+	low=soup.find_all('td',{'class':'align_right climate_table_data_td temperature_blue'})
+	other_three=soup.find_all('td',{'class':'align_right climate_table_data_td'})
+	record={}
+	month=str(m)
+	if m<10:
+		month='0'+month
+	for i in range(0,len(day)):
+		if i<9:
+			date=str(y)+month+'0'+str(i+1)
+		else:
+			date=str(y)+month+str(i+1)
+		record[date]={
+					'high':high[i].string.encode('utf'),
+					'low':low[i].string.encode('utf'),
+					'preception':other_three[3*i].string.encode('utf')
+					}
+	return(record)
+
+for y in year:
+	for m in month:
+		print y,m
+		record.update(get_table(y,m))
+f=open('/Users/sheepfriend/data.csv','w')
+f.write("'date';'high';'low';'preception'\n")
+for key in sorted(record):
+	value=record[key]
+	f.write(key+";"+value['high']+";"+value['low']+";"+value['preception']+"\n")
+f.close()
